5e8faad2f880856a659b84831fcd98aeed72b924
Cthuko Whitebear <manatsu.yuhara@gmail.com>
2014-10-30 19:26:10 +0100

    More work on tile placing
    check for button, hide on mouse up

--- States/Main.js
+++ States/Main.js
@@ -24,11 +24,13 @@ g.States.Main = {
     this.game.add.existing(this.levelTilePlacing);
 
     this.game.input.onDown.add(this.levelClick, this);
+    this.game.input.onUp.add(this.levelMouseUp, this);
   },
 
   tileSelectionChanged: function() {
     this.levelTilePlacing.updateCrop();
     this.placeMode = "placing";
+    this.levelTilePlacing.visible = true;
   },
 
   setOpenLevel: function(level) {
@@ -54,13 +56,30 @@ g.States.Main = {
     this.levelTilePlacing.updateCrop();
   },
 
-  levelClick: function() {
-    if (this.openLevel == null) { return; }
+  getPointerTile: function() {
     var point = Phaser.Canvas.getOffset(this.game.canvas);
     var pointerTileX = Math.round((this.game.input.mousePointer.pageX - point.x) / 16);
     var pointerTileY = Math.round((this.game.input.mousePointer.pageY - point.y) / 16);
+    return [pointerTileX, pointerTileY];
+  },
 
-    this.openLevel.placeTiles(pointerTileX - this.tileSelection.selectionRect.w, pointerTileY - this.tileSelection.selectionRect.h, this.getSelectedTileArray(), this.activeLayer);
+  levelClick: function() {
+    if (this.openLevel == null) { return; }
+    if (this.placeMode !== "placing") { return; }
+
+    if (this.game.input.mouse.button === 0) {
+      var pointerTile = this.getPointerTile();
+      this.openLevel.placeTiles(pointerTile[0] - this.tileSelection.selectionRect.w, pointerTile[1] - this.tileSelection.selectionRect.h, this.getSelectedTileArray(), this.activeLayer);
+    } else if (this.gam.einput.mouse.button === 2) {
+      this.openLevel.floodFill(pointerTile[0] - this.tileSelection.selectionRect.w, pointerTile[1] - this.tileSelection.selectionRect.h, this.getSelectedTileArray()[0][0], this.activeLayer);
+    }
+  },
+
+  levelMouseUp: function() {
+    var pointerTile = this.getPointerTile();
+
+    this.placeMode = "none";
+    this.levelTilePlacing.visible = false;
   },
 
   getSelectedTileArray: function() {
@@ -78,12 +97,10 @@ g.States.Main = {
     if (this.placeMode === "placing") {
       if (this.levelTilePlacing.visible) {
 
-        var point = Phaser.Canvas.getOffset(this.game.canvas);
-        var pointerTileX = Math.round((this.game.input.mousePointer.pageX - point.x) / 16);
-        var pointerTileY = Math.round((this.game.input.mousePointer.pageY - point.y) / 16);
+        var pointerTile = this.getPointerTile();
 
-        this.levelTilePlacing.x = pointerTileX * 16 - this.tileSelection.cropRect.width;
-        this.levelTilePlacing.y = pointerTileY * 16 - this.tileSelection.cropRect.height;
+        this.levelTilePlacing.x = pointerTile[0] * 16 - this.tileSelection.cropRect.width;
+        this.levelTilePlacing.y = pointerTile[1] * 16 - this.tileSelection.cropRect.height;
         this.levelTilePlacing.bringToTop();
       }
     }
