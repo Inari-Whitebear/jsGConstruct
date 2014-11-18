7d71abb92ba8c76be63d1fdc8520e68d2a82f7cb
Cthuko Whitebear <manatsu.yuhara@gmail.com>
2014-11-18 18:21:54 +0100

    add (messy) support for floodfill undo/redo
    todo: cleanup sometime

--- Prefabs/Level.js
+++ Prefabs/Level.js
@@ -116,7 +116,9 @@ Level.prototype.placeTiles = function(x, y, tileArray, layer, noHistory) {
     org[tX] = [];
     for (var tY = 0, tYL = tileArray[0].length; tY < tYL; tY++) {
       org[tX][tY] = this.tileMap.getTile(tX + x, tY + y, layer, true).index;
-      this.tileMap.putTile(tileArray[tX][tY], tX + x, tY + y, layer);
+      if (tileArray[tX][tY] !== -2) {
+        this.tileMap.putTile(tileArray[tX][tY], tX + x, tY + y, layer);
+      }
     }
   }
 
@@ -128,11 +130,61 @@ Level.prototype.placeTiles = function(x, y, tileArray, layer, noHistory) {
   g.manager.updateTab(this);
 };
 
-var vecx = function(dir) { if (dir === 1) { return 1; } else if(dir === 3) { return -1; } return 0; }
-var vecy = function(dir) { if (dir === 0) { return -1; } else if(dir === 2) { return 1; } return 0; }
-Level.prototype.floodFill = function(x, y, tile, layer, fillIndex) {
+var vecx = function(dir) { if (dir === 1) { return 1; } else if(dir === 3) { return -1; } return 0; };
+var vecy = function(dir) { if (dir === 0) { return -1; } else if(dir === 2) { return 1; } return 0; };
+var addRow = function(array, index) {
+  for (var i = 0, l = array.length; i < l; i++) {
+    array[i].splice(index, 0, -2);
+  }
+};
+var addColumn = function(array, index) {
+  var length = array[0].length;
+  var row = [];
+  for(var i = 0; i < length; i++) {
+    row.push(-2);
+  }
+  array.splice(index, 0, row);
+};
+Level.prototype.floodFill = function(x, y, tile, layer, noHistory, floodFillInfo) {
+  if (floodFillInfo == null) {
+    floodFillInfo = {};
+    floodFillInfo.top = y;
+    floodFillInfo.left = x;
+    floodFillInfo.width = 1;
+    floodFillInfo.height = 1;
+    floodFillInfo.fillIndex = this.tileMap.getTile(x, y, layer, true).index;
+    floodFillInfo.data = [[-2]];
+    floodFillInfo.orgData = [[-2]];
+  }
+  if (noHistory == null) { noHistory = false; }
+
+
+  if (x < floodFillInfo.left) {
+    floodFillInfo.left--;
+    floodFillInfo.width++;
+    addColumn(floodFillInfo.data, 0);
+    addColumn(floodFillInfo.orgData, 0);
+  } else if (y < floodFillInfo.top) {
+    floodFillInfo.top--;
+    floodFillInfo.height++;
+    addRow(floodFillInfo.data, 0);
+    addRow(floodFillInfo.orgData, 0);
+  }
+
+  if (x > floodFillInfo.left + floodFillInfo.width - 1) {
+    floodFillInfo.width++;
+    addColumn(floodFillInfo.data, floodFillInfo.width - 1);
+    addColumn(floodFillInfo.orgData, floodFillInfo.width - 1);
+  } else if (y > floodFillInfo.top + floodFillInfo.height - 1) {
+    floodFillInfo.height++;
+    addRow(floodFillInfo.data, floodFillInfo.height - 1);
+    addRow(floodFillInfo.orgData, floodFillInfo.height - 1);
+  }
+
+  floodFillInfo.data[x - floodFillInfo.left][y - floodFillInfo.top] = tile;
   var tileIndex;
-  if (fillIndex == null ) { fillIndex = this.tileMap.getTile(x, y, layer, true).index; }
+  tileIndex = this.tileMap.getTile(x, y, layer, true).index;
+  floodFillInfo.orgData[x - floodFillInfo.left][y - floodFillInfo.top] = tileIndex;
   this.tileMap.putTile(tile, x, y, layer);
   var tX, tY;
   for (var i = 0; i < 4; i++) {
@@ -141,11 +193,15 @@ Level.prototype.floodFill = function(x, y, tile, layer, fillIndex) {
     if (tX >= 0 && tY >= 0 && tX < this.width && tY < this.height) {
       tileIndex = this.tileMap.getTile(tX, tY, layer, true).index;
       if (tileIndex !== -1) {
-        if (tileIndex === fillIndex) {
-          this.floodFill(tX, tY, tile, layer, fillIndex);
+        if (tileIndex === floodFillInfo.fillIndex) {
+          this.floodFill(tX, tY, tile, layer, true, floodFillInfo);
         }
       }
     }
+  }
+
+  if (!noHistory) {
+    this.historyManager.saveStep(floodFillInfo.left, floodFillInfo.top, floodFillInfo.orgData, floodFillInfo.data, layer);
   }
 };
 
